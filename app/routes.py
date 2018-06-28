f90fcdc2262696303a6c58cdd7808549838c6dca
Konstantinov Innokentii <konstantinovksh@gmail.com>
2018-06-28 12:26:04 +0500

    Use dinner view instead of food daemoon.

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,17 @@
 from app import app
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return "Hello, World!"
+import datetime
+from flask_sqlalchemy_session import current_session
+from app.models import Dinner
+
+
+@app.route("/dinners", methods=["GET"])
+def check_dinner():
+    dinners = current_session.query(Dinner).all()
+
+    for dinner in dinners:
+        dt = datetime.datetime.now().time().replace(second=0, microsecond=0)
+        if dinner.date == dt:
+            return str(dinner.size)
+        else:
+            return '0'
